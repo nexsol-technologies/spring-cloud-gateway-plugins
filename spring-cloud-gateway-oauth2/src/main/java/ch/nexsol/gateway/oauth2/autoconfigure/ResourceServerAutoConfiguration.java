5bb25e887699d9288ac864632d2df8b9a7f32238
guerricm <guerric.merle@nexsol.tech>
2024-09-05 22:47:13 +0200

    add module spring-cloud-gateway-oauth2

---
+++ spring-cloud-gateway-oauth2/src/main/java/ch/nexsol/gateway/oauth2/autoconfigure/ResourceServerAutoConfiguration.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.nexsol.gateway.oauth2.autoconfigure;
+
+import ch.nexsol.gateway.oauth2.resourceserver.DefaultJwtGrantedAuthoritiesConverter;
+import ch.nexsol.gateway.oauth2.resourceserver.multitenancy.ResourceServerMultiTenantProperties;
+import ch.nexsol.gateway.oauth2.resourceserver.multitenancy.TrustedIssuerJwtReactiveAuthenticationManagerResolver;
+import ch.nexsol.gateway.oauth2.resourceserver.multitenancy.condition.MultitenancyConfiguredCondition;
+import ch.nexsol.gateway.oauth2.resourceserver.multitenancy.condition.OnMissingMultitenancyConfiguredCondition;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.AllNestedConditions;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.IssuerUriCondition;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.ReactiveAuthenticationManagerResolver;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerReactiveAuthenticationManagerResolver;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.server.ServerWebExchange;
+
+@AutoConfiguration
+public class ResourceServerAutoConfiguration {
+
+	@Bean
+	@ConditionalOnMissingBean(Converter.class)
+	Converter<Jwt, AbstractAuthenticationToken> defaultJwtGrantedAuthoritiesConverter(
+			@Value("${spring.application.name}") String resourceName) {
+		return new DefaultJwtGrantedAuthoritiesConverter(resourceName);
+	}
+
+	@Bean
+	@Conditional(MultitenancyConfiguredCondition.class)
+	@ConfigurationProperties("spring.security.oauth2.resourceserver")
+	ResourceServerMultiTenantProperties defaultOAuth2ResourceServerMultiTenantProperties() {
+		return new ResourceServerMultiTenantProperties();
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnWebApplication(type = Type.REACTIVE)
+	@ConditionalOnClass({ WebFluxConfigurer.class })
+	public static class ReactiveOAuth2MultitenancyConfiguration {
+
+		@Bean
+		@Conditional(MultitenancyConfiguredCondition.class)
+		ReactiveAuthenticationManagerResolver<ServerWebExchange> jwtIssuerReactiveAuthenticationManagerResolver(
+				ResourceServerMultiTenantProperties resourceServerMultiTenantProperties,
+				DefaultJwtGrantedAuthoritiesConverter converter) {
+
+			return new JwtIssuerReactiveAuthenticationManagerResolver(
+					new TrustedIssuerJwtReactiveAuthenticationManagerResolver(
+							resourceServerMultiTenantProperties.getIssuerUri()::contains,
+							new ReactiveJwtAuthenticationConverterAdapter(converter)));
+
+		}
+
+		@Bean
+		@Conditional(DefaultResourceserverConfigurationCondition.class)
+		ReactiveAuthenticationManagerResolver<ServerWebExchange> defaultJwtIssuerReactiveAuthenticationManagerResolver(
+				OAuth2ResourceServerProperties properties, Converter<Jwt, AbstractAuthenticationToken> converter) {
+
+			return new JwtIssuerReactiveAuthenticationManagerResolver(
+					new TrustedIssuerJwtReactiveAuthenticationManagerResolver(
+							properties.getJwt().getIssuerUri()::equals,
+							new ReactiveJwtAuthenticationConverterAdapter(converter)));
+		}
+
+	}
+
+	static class DefaultResourceserverConfigurationCondition extends AllNestedConditions {
+
+		DefaultResourceserverConfigurationCondition() {
+			super(ConfigurationPhase.REGISTER_BEAN);
+		}
+
+		@Conditional(IssuerUriCondition.class)
+		static class DefaultConfig {
+
+		}
+
+		@Conditional(OnMissingMultitenancyConfiguredCondition.class)
+		static class NoMultiTenancy {
+
+		}
+
+	}
+
+}
---
+++ spring-cloud-gateway-oauth2/src/main/java/ch/nexsol/gateway/oauth2/resourceserver/multitenancy/ResourceServerMultiTenantProperties.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.nexsol.gateway.oauth2.resourceserver.multitenancy;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public class ResourceServerMultiTenantProperties {
+
+	/**
+	 * List of multi-tenant
+	 */
+	@NotEmpty
+	@Valid
+	private List<@NotNull OAuth2ResourceServerProperties> multitenant = new ArrayList<>();
+
+	/**
+	 * @return the multi-tenant
+	 */
+	public List<OAuth2ResourceServerProperties> getMultitenant() {
+		return this.multitenant;
+	}
+
+	/**
+	 * @param multitenant the multi-tenant to set
+	 */
+	public void setMultitenant(List<OAuth2ResourceServerProperties> multitenant) {
+		this.multitenant = multitenant;
+	}
+
+	/**
+	 * @return the list of issuer uri configured
+	 */
+	public List<String> getIssuerUri() {
+		if (this.multitenant != null) {
+			return this.multitenant.stream()
+				.map((property) -> property.getIssuerUri())
+				.map((issuerUri) -> issuerUri.toString())
+				.collect(Collectors.toUnmodifiableList());
+		}
+		return Collections.emptyList();
+	}
+
+	/**
+	 * @return true if some issuer uri are configured
+	 */
+	public boolean isMultitenantEnabled() {
+		return !this.multitenant.isEmpty();
+	}
+
+	public static class OAuth2ResourceServerProperties {
+
+		/**
+		 * identifier of the tenant
+		 */
+		@NotEmpty
+		private String id;
+
+		/**
+		 * the issuer uri of the tenant
+		 */
+		@NotEmpty
+		private URI issuerUri;
+
+		/**
+		 * @return the id
+		 */
+		public String getId() {
+			return this.id;
+		}
+
+		/**
+		 * @param id the id to set
+		 */
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		/**
+		 * @return the issuerUri
+		 */
+		public URI getIssuerUri() {
+			return this.issuerUri;
+		}
+
+		/**
+		 * @param issuerUri the issuerUri to set
+		 */
+		public void setIssuerUri(URI issuerUri) {
+			this.issuerUri = issuerUri;
+		}
+
+	}
+
+}
