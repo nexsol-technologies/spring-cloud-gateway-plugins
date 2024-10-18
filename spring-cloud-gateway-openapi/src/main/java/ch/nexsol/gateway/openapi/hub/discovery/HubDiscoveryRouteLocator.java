b4d440693d39e3426662d520e993b0f60a648bc2
guerricm <guerric.merle@nexsol.tech>
2024-10-18 15:36:21 +0200

    feat: openapi

---
+++ spring-cloud-gateway-openapi/src/main/java/ch/nexsol/gateway/openapi/hub/discovery/HubDiscoveryRouteLocator.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.nexsol.gateway.openapi.hub.discovery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import ch.nexsol.gateway.openapi.hub.OpenapiService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import reactor.core.publisher.Flux;
+
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.filter.FilterDefinition;
+import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.context.event.EventListener;
+import org.springframework.util.StringUtils;
+
+public class HubDiscoveryRouteLocator extends DiscoveryClientRouteDefinitionLocator {
+
+	private static final Logger LOG = LoggerFactory.getLogger(HubDiscoveryRouteLocator.class);
+
+	private static final String API_DOCS_URL = "/v3/api-docs";
+
+	private static final String ROUTE_ID_PREFIX = "openapi-docs-discovery-";
+
+	private final SwaggerUiConfigParameters swaggerUiConfigParameters;
+
+	private final OpenapiService openapiService;
+
+	// because it's private in DiscoveryClientRouteDefinitionLocator
+	private final String routeIdPrefix;
+
+	public HubDiscoveryRouteLocator(ReactiveDiscoveryClient discoveryClient, DiscoveryLocatorProperties properties,
+			OpenapiService openapiService, SwaggerUiConfigParameters swaggerUiConfigParameters) {
+		super(discoveryClient, properties);
+		this.openapiService = openapiService;
+		this.swaggerUiConfigParameters = swaggerUiConfigParameters;
+
+		if (StringUtils.hasText(properties.getRouteIdPrefix())) {
+			this.routeIdPrefix = properties.getRouteIdPrefix();
+		}
+		else {
+			this.routeIdPrefix = discoveryClient.getClass().getSimpleName() + "_";
+		}
+	}
+
+	@Override
+	public Flux<RouteDefinition> getRouteDefinitions() {
+		return super.getRouteDefinitions()
+			// do not process existing api doc routes
+			.filter((route) -> !route.getId().startsWith(ROUTE_ID_PREFIX))
+			.flatMap((routeDefinition) -> {
+				String routeId = routeDefinition.getId().replace(this.routeIdPrefix, "");
+				return this.openapiService.discoverOpenapiUrl(routeId, routeDefinition);
+
+			})
+			.map((openapiDiscover) -> {
+				RouteDefinition routeDefinition = openapiDiscover.routeDefinition();
+
+				// the path to the route to the microservice (ex. lb://MICROSERVICE-A)
+				String path = "/" + routeDefinition.getUri().getHost();
+
+				String name = routeDefinition.getId();
+				name = name.replace(this.routeIdPrefix, "");
+
+				// create OPENAPI route
+				RouteDefinition r = new RouteDefinition();
+				r.setId(ROUTE_ID_PREFIX + name);
+				r.setUri(routeDefinition.getUri());
+				r.setMetadata(Map.of("name", name));
+
+				// filter phase
+				List<FilterDefinition> filters = new ArrayList<>();
+				FilterDefinition f = new FilterDefinition("RewritePath=" + API_DOCS_URL + path + ", " + API_DOCS_URL);
+				filters.add(f);
+				f = new FilterDefinition("OpenapiModifyResponseBody=" + path);
+				filters.add(f);
+				r.setFilters(filters);
+
+				// predicate phase
+				List<PredicateDefinition> predicates = new ArrayList<>();
+				PredicateDefinition p = new PredicateDefinition("Path=" + API_DOCS_URL + path);
+				predicates.add(p);
+				r.setPredicates(predicates);
+
+				LOG.debug("Create openapi route {} for existing discovery route {}", r.toString(),
+						routeDefinition.toString());
+
+				return r;
+			});
+	}
+
+	@EventListener
+	public void handleContextStart(RefreshRoutesEvent event) {
+
+		this.swaggerUiConfigParameters.setUrls(new java.util.HashSet<>());
+		this.swaggerUiConfigParameters.addGroup("-- Choose --");
+
+		this.getRouteDefinitions().filter((route) -> route.getId().startsWith(ROUTE_ID_PREFIX)).doOnNext((route) -> {
+			String name = (String) route.getMetadata().get("name");
+			GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+			this.swaggerUiConfigParameters.addGroup(name);
+		}).subscribe();
+
+	}
+
+}
---
+++ spring-cloud-gateway-openapi/src/main/java/ch/nexsol/gateway/openapi/hub/filter/OpenapiModifyResponseBodyGatewayFilterFactory.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.nexsol.gateway.openapi.hub.filter;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.models.servers.Server;
+import reactor.core.publisher.Mono;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.rewrite.MessageBodyDecoder;
+import org.springframework.cloud.gateway.filter.factory.rewrite.MessageBodyEncoder;
+import org.springframework.cloud.gateway.filter.factory.rewrite.ModifyResponseBodyGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.rewrite.RewriteFunction;
+import org.springframework.http.codec.HttpMessageReader;
+import org.springframework.util.Assert;
+
+public class OpenapiModifyResponseBodyGatewayFilterFactory
+		extends AbstractGatewayFilterFactory<OpenapiModifyResponseBodyGatewayFilterFactory.Config> {
+
+	private final ObjectMapper objectMapper;
+
+	private final Set<MessageBodyDecoder> messageBodyDecoders;
+
+	private final Set<MessageBodyEncoder> messageBodyEncoders;
+
+	private final List<HttpMessageReader<?>> messageReaders;
+
+	private final URI apiGatewayUri;
+
+	/**
+	 * Path key.
+	 */
+	public static final String PATH_KEY = "path";
+
+	@Override
+	public List<String> shortcutFieldOrder() {
+		return Arrays.asList(PATH_KEY);
+	}
+
+	public OpenapiModifyResponseBodyGatewayFilterFactory(List<HttpMessageReader<?>> messageReaders,
+			Set<MessageBodyDecoder> messageBodyDecoders, Set<MessageBodyEncoder> messageBodyEncoders,
+			URI apiGatewayUri) {
+		super(Config.class);
+
+		this.objectMapper = new ObjectMapper();
+		this.objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+		this.messageReaders = messageReaders;
+		this.messageBodyDecoders = messageBodyDecoders;
+		this.messageBodyEncoders = messageBodyEncoders;
+
+		this.apiGatewayUri = apiGatewayUri;
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		org.springframework.cloud.gateway.filter.factory.rewrite.ModifyResponseBodyGatewayFilterFactory.Config c = new org.springframework.cloud.gateway.filter.factory.rewrite.ModifyResponseBodyGatewayFilterFactory.Config();
+		c.setRewriteFunction(LinkedHashMap.class, String.class, rewriteServersWithGatewayUrl(config.getPath()));
+		ModifyResponseBodyGatewayFilterFactory factory = new ModifyResponseBodyGatewayFilterFactory(this.messageReaders,
+				this.messageBodyDecoders, this.messageBodyEncoders);
+
+		ModifyResponseBodyGatewayFilterFactory.ModifyResponseGatewayFilter gatewayFilter = factory.new ModifyResponseGatewayFilter(
+				c);
+		return gatewayFilter;
+	}
+
+	public static class Config {
+
+		private String path;
+
+		public String getPath() {
+			return this.path;
+		}
+
+		public Config setPath(String path) {
+			Assert.hasText(path, "path must have a value");
+			this.path = path;
+			return this;
+		}
+
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private RewriteFunction<LinkedHashMap, String> rewriteServersWithGatewayUrl(String path) {
+		return (serverWebExchange, openAPI) -> {
+
+			if (openAPI == null) {
+				return Mono.empty();
+			}
+			Server server = new Server();
+			server.setUrl(this.apiGatewayUri.toString() + path);
+			openAPI.put("servers", Collections.singletonList(server));
+			String result;
+			try {
+				result = this.objectMapper.writeValueAsString(openAPI);
+				return Mono.just(result);
+			}
+			catch (JsonProcessingException ex) {
+				return Mono.empty();
+			}
+		};
+	}
+
+}
