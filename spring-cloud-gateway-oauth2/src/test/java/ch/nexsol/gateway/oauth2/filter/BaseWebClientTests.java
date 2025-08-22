cff4c2786b94da2c3fd76c513948f208d0a41755
guerricm <guerric.merle@nexsol.tech>
2025-08-22 10:10:57 +0200

    fix: test managements

---
+++ spring-cloud-gateway-oauth2/src/test/java/ch/nexsol/gateway/oauth2/filter/BaseWebClientTests.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.nexsol.gateway.oauth2.filter;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * @see <a href=
+ * "https://github.com/spring-cloud/spring-cloud-gateway/blob/main/spring-cloud-gateway-server/src/test/java/org/springframework/cloud/gateway/test/BaseWebClientTests.java">spring-cloud-gateway
+ * BaseWebClientTests</a>
+ */
+public class BaseWebClientTests {
+
+	protected String baseUri;
+
+	@LocalServerPort
+	protected int port = 0;
+
+	protected WebTestClient testClient;
+
+	@BeforeEach
+	public void setup() throws Exception {
+		setup(new ReactorClientHttpConnector(), "http://localhost:" + this.port);
+	}
+
+	protected void setup(ClientHttpConnector httpConnector, String baseUri) {
+		this.baseUri = baseUri;
+		this.testClient = WebTestClient.bindToServer(httpConnector).baseUrl(this.baseUri).build();
+	}
+
+}
